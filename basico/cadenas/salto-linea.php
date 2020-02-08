16911f3958677cd9bafcdc77b97dea90e863a1f7
Víctor Cuervo <vcuervo@gmail.com>
2020-02-08 14:20:18 +0100

    Salto de línea en cadenas

---
+++ basico/cadenas/salto-linea.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file salto-linea.php
+ * @version 1.0
+ * @author Víctor Cuervo (http://lineadecodigo.com)
+ * @date   08/febrero/2020
+ * @url    http://lineadecodigo.com/php/pdte/
+ * @description Crear un salto de línea en una cadena
+ */
+
+
+echo "Principio de la frase \n";
+echo "Final de la frase \n";
+
+?>
