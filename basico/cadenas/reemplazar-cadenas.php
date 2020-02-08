5a3e3b536e67fa5ee75eb73852ee9c6e412908bb
Víctor Cuervo <vcuervo@gmail.com>
2020-02-08 14:21:30 +0100

    Reemplazar caracteres en una cadena por números.

---
+++ basico/cadenas/reemplazar-cadenas.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file reemplazar-cadena.php
+ * @version 1.0
+ * @author Víctor Cuervo (http://lineadecodigo.com)
+ * @date   08/febrero/2020
+ * @url    http://lineadecodigo.com/php/pdte/
+ * @description Ejemplo que sustituye un caracter 'x' dentro de una cadena por un número.
+ */
+
+
+$lista = '12345xxxxx';
+echo 'Cadena Inicial: '.$lista."\n";
+
+while (strpos($lista, 'x')) {
+	$lista = preg_replace('/x/', rand(0,9), $lista, 1);
+}
+
+echo 'Cadena Final: '.$lista;
+?>
