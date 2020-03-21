3df87909ff635f3ee17ae58bd38d825d037c0abb
Víctor Cuervo <vcuervo@gmail.com>
2020-03-21 12:28:16 +0100

    Añadida documentación.

--- basico/cadenas/salto-linea.php
+++ basico/cadenas/salto-linea.php
@@ -4,7 +4,7 @@
  * @version 1.0
  * @author Víctor Cuervo (http://lineadecodigo.com)
  * @date   08/febrero/2020
- * @url    http://lineadecodigo.com/php/pdte/
+ * @url    http://lineadecodigo.com/php/saltos-de-linea-en-php/
  * @description Crear un salto de línea en una cadena
  */
 
