d50bef0028511e5505f4bcfce8021cf4dca14fee
victorcuervo <vcuervo@gmail.com>
2025-01-14 01:10:16 +0100

    Documentación operador nave espacial

--- basico/operador-nave-espacial.php
+++ basico/operador-nave-espacial.php
@@ -4,7 +4,7 @@
  * @version 1.0
  * @author Linea de Codigo (http://lineadecodigo.com)
  * @date   28/enero/2024
- * @url    http://lineadecodigo.com/php/xxx/
+ * @url    https://lineadecodigo.com/blog/operador-nave-espacial-spaceship-en-php/
  * @description Cómo utilizar el operador nave espacial <=>
  */
 
@@ -22,4 +22,13 @@ $n2 = 1;
     case 1:
         echo "$n1 es más grande";
  }
- 
+
+$numeros = [1,2,3,4,5,6,7,8,9,10];
+shuffle($numeros);
+print_r($numeros);
+
+usort($numeros, function ($a, $b) {
+    return $a <=> $b; // Ordenar de menor a mayor
+});
+
+print_r($numeros);
